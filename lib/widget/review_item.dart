bc575876f0e28140d6fc360b33912afc18c3e94d
Alex Rodrigues <alex.rodrigues.developer@gmail.com>
2021-07-03 21:26:19 -0300

    fixing screen reload

--- lib/widget/review_item.dart
+++ lib/widget/review_item.dart
@@ -10,39 +10,33 @@ class ReviewItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: _review.imageUrl.isNotEmpty
-          ? Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Card(
-                child: ListTile(
-                  leading: CircleAvatar(
-                    child: Image.network(
-                      _review.imageUrl,
-                      fit: BoxFit.cover,
-                    ),
+          ? Card(
+              child: ListTile(
+                leading: CircleAvatar(
+                  child: Image.network(
+                    _review.imageUrl,
+                    fit: BoxFit.cover,
                   ),
-                  title: Text(
-                    _review.name,
-                    style: TextStyle(fontSize: 13.0),
-                  ),
-                  subtitle: Text(
-                    _review.content,
-                    style: TextStyle(fontSize: 11.0, color: Colors.grey),
-                  ),
+                ),
+                title: Text(
+                  _review.name,
+                  style: TextStyle(fontSize: 13.0),
+                ),
+                subtitle: Text(
+                  _review.content,
+                  style: TextStyle(fontSize: 11.0, color: Colors.grey),
                 ),
               ),
             )
-          : Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Card(
-                child: ListTile(
-                  title: Text(
-                    _review.name,
-                    style: TextStyle(fontSize: 13.0),
-                  ),
-                  subtitle: Text(
-                    _review.content,
-                    style: TextStyle(fontSize: 11.0, color: Colors.grey),
-                  ),
+          : Card(
+              child: ListTile(
+                title: Text(
+                  _review.name,
+                  style: TextStyle(fontSize: 13.0),
+                ),
+                subtitle: Text(
+                  _review.content,
+                  style: TextStyle(fontSize: 11.0, color: Colors.grey),
                 ),
               ),
             ),
